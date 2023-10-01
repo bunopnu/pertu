afae120abeebcd8bc629649606ba35bbb10894d0
bun <git@bun.rip>
2023-10-01 12:12:08 +0300

    add auto-create .pertu folder and add to path

--- src/Pertu.ps1
+++ src/Pertu.ps1
@@ -1,3 +1,4 @@
+# Check command-line arguments
 if ($args.Count -lt 1) {
   Write-Error "Please provide a manager to use Pertu!"
   Exit
@@ -7,6 +8,20 @@ elseif ($args.Count -lt 2) {
   Exit
 }
 
+# Get the home directory and environment path
+$binDirectory = "$env:USERPROFILE\.pertu\bin"
+$environmentPath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User)
+
+# Create necessary folders if it doesn't exist
+[void](New-Item -ItemType Directory -Path $binDirectory -Force)
+
+# Add .pertu/bin to the environment path if not already
+if (-Not ($environmentPath.Contains($binDirectory))) {
+  $environmentPath += ";$binDirectory"
+  [Environment]::SetEnvironmentVariable("Path", $environmentPath, [EnvironmentVariableTarget]::User)
+}
+
+# Load manager and action
 try {
   $manager = $args[0]
   $action = $args[1]
