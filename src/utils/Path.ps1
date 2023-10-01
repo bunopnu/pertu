8aa778f0798b0571cdf5e131cf4ce3ba215e2d35
tuhana <git@tuhana.me>
2023-10-01 23:31:35 +0300

    use `%LocalAppData%` instead of `%UserProfile%`

--- src/utils/Path.ps1
+++ src/utils/Path.ps1
@@ -1,19 +1,19 @@
 # Function to get the Pertu bin directory
 function Get-PertuBinDirectory() {
-  return "$env:USERPROFILE\.pertu\bin"
+  return "$env:LocalAppData\.pertu\bin"
 }
 
 # Function to get the directory for a Pertu manager
 function Get-PertuManagerDirectory($manager) {
-  return "$env:USERPROFILE\.pertu\$manager"
+  return "$env:LocalAppData\.pertu\$manager"
 }
 
 # Function to get the directory for a specific version of a Pertu manager
 function Get-PertuManagerVersionDirectory($manager, $version) {
-  return "$env:USERPROFILE\.pertu\$manager\$version"
+  return "$env:LocalAppData\.pertu\$manager\$version"
 }
 
 # Function to get the directory for a specific GitHub repository version
 function Get-GitHubRepositoryDirectory($manager, $repository, $version) {
-  return "$env:USERPROFILE\.pertu\$manager\$version\$repository-$version"
+  return "$env:LocalAppData\.pertu\$manager\$version\$repository-$version"
 }
