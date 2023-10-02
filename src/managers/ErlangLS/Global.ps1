2411c81dc30b9e462925da499962b9b05d4f8ee9
tuhana <git@tuhana.me>
2023-10-02 14:03:20 +0300

    fix `remove` and `global` sub-commands

--- src/managers/ErlangLS/Global.ps1
+++ src/managers/ErlangLS/Global.ps1
@@ -1,7 +1,7 @@
 using module ..\..\utils\Action.psm1
 
 # Find the specified version argument in the command-line arguments
-$version = Find-VersionArgument $args
+$version = Find-VersionFromArgument $args "erlang-ls/erlang_ls"
 
 # Add the specified version of ErlangLS to the environment
 Copy-ManagerToBin "erlang-ls" $version
--- src/managers/Gleam/Remove.ps1
+++ src/managers/Gleam/Remove.ps1
@@ -1,7 +1,7 @@
 using module ..\..\utils\Action.psm1
 
 # Find the specified version argument in the command-line arguments
-$version = Find-VersionArgument $args
+$version = Find-VersionFromArgument $args "gleam-lang/gleam"
 
 # Remove the specified version of the Gleam
 Remove-ManagerVersion "gleam" $version @("gleam.exe")
