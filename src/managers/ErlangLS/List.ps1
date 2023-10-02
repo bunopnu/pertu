cd4483a1b92831a79a1836b2de0788272678db22
tuhana <git@tuhana.me>
2023-10-02 13:15:22 +0300

    fix `list` command on ErlangLS manager

--- src/managers/ErlangLS/List.ps1
+++ src/managers/ErlangLS/List.ps1
@@ -2,4 +2,4 @@ using module ..\..\utils\Path.psm1
 using module ..\..\utils\Version.psm1
 
 # Write the list of available versions for ErlangLS
-Write-AvailableVersions "erlang_ls"
+Write-AvailableVersions "erlang-ls"
